56d5deea77f76034f3970405d75213d1a30080b1
Chang <olifant.xiang@gmail.com>
2022-09-12 16:32:33 +0800

    Initial Commit

---
+++ Shared/TickTockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTockApp.swift
+//  Shared
+//
+//  Created by Chang Chu on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TickTockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
